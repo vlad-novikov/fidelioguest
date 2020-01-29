658efb4bce3fdb246725100b051d41abe92f70c5
Vlad Novikov <vlad.novikov@gmail.com>
2020-01-29 22:22:51 +0300

    Clipboard check hook is working

--- FidelioGuest/MainWindow.xaml.cs
+++ FidelioGuest/MainWindow.xaml.cs
@@ -26,98 +26,98 @@ namespace FidelioGuest
     {
 
 
-        public class ClipboardMonitorWindow : Window
+
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+        private IntPtr windowHandle;
+
+        public event EventHandler ClipboardUpdate;
+
+        protected override void OnInitialized(EventArgs e)
         {
-            private const int WM_CLIPBOARDUPDATE = 0x031D;
+            base.OnInitialized(e);
 
-            private IntPtr windowHandle;
-
-            public event EventHandler ClipboardUpdate;
-
-            protected override void OnInitialized(EventArgs e)
-            {
-                base.OnInitialized(e);
-
-                windowHandle = new WindowInteropHelper(this).EnsureHandle();
-                HwndSource.FromHwnd(windowHandle)?.AddHook(HwndHandler);
-                Start();
-            }
-
-            public static readonly DependencyProperty ClipboardUpdateCommandProperty =
-                DependencyProperty.Register("ClipboardUpdateCommand", typeof(ICommand), typeof(ClipboardMonitorWindow), new FrameworkPropertyMetadata(null));
-
-            public ICommand ClipboardUpdateCommand
-            {
-                get { return (ICommand)GetValue(ClipboardUpdateCommandProperty); }
-                set { SetValue(ClipboardUpdateCommandProperty, value); }
-            }
-
-            protected virtual void OnClipboardUpdate()
-            { }
-
-            public void Start()
-            {
-                NativeMethods.AddClipboardFormatListener(windowHandle);
-            }
-
-            public void Stop()
-            {
-                NativeMethods.RemoveClipboardFormatListener(windowHandle);
-            }
-
-            private IntPtr HwndHandler(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
-            {
-                int a=1;
-                if (msg == WM_CLIPBOARDUPDATE)
-                {
-                    
-                    // fire event
-                    this.ClipboardUpdate?.Invoke(this, new EventArgs());
-                    // execute command
-                    if (this.ClipboardUpdateCommand?.CanExecute(null) ?? false)
-                    {
-                        this.ClipboardUpdateCommand?.Execute(null);
-                    }
-                    // call virtual method
-                    OnClipboardUpdate();
-                }
-                handled = false;
-                return IntPtr.Zero;
-            }
-
-
-            private static class NativeMethods
-            {
-                [DllImport("user32.dll", SetLastError = true)]
-                [return: MarshalAs(UnmanagedType.Bool)]
-                public static extern bool AddClipboardFormatListener(IntPtr hwnd);
-
-                [DllImport("user32.dll", SetLastError = true)]
-                [return: MarshalAs(UnmanagedType.Bool)]
-                public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
-            }
-
-
-
+            windowHandle = new WindowInteropHelper(this).EnsureHandle();
+            HwndSource.FromHwnd(windowHandle)?.AddHook(HwndHandler);
+            Start();
         }
+
+        public static readonly DependencyProperty ClipboardUpdateCommandProperty =
+            DependencyProperty.Register("ClipboardUpdateCommand", typeof(ICommand), typeof(MainWindow), new FrameworkPropertyMetadata(null));
+
+        public ICommand ClipboardUpdateCommand
+        {
+            get { return (ICommand)GetValue(ClipboardUpdateCommandProperty); }
+            set { SetValue(ClipboardUpdateCommandProperty, value); }
+        }
+
+        protected virtual void OnClipboardUpdate()
+        { }
+
+        public void Start()
+        {
+            NativeMethods.AddClipboardFormatListener(windowHandle);
+        }
+
+        public void Stop()
+        {
+            NativeMethods.RemoveClipboardFormatListener(windowHandle);
+        }
+
+        private IntPtr HwndHandler(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {       
+            if (msg == WM_CLIPBOARDUPDATE)
+            {
+
+                // fire event
+                txtResID.Text = "Clipboard updated";
+
+                this.ClipboardUpdate?.Invoke(this, new EventArgs());
+                // execute command
+                if (this.ClipboardUpdateCommand?.CanExecute(null) ?? false)
+                {
+                    this.ClipboardUpdateCommand?.Execute(null);
+                }
+                // call virtual method
+                OnClipboardUpdate();
+            }
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+
+        private static class NativeMethods
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+        }
+
+
+
+
         ///////////////////////////////////////////////
-        
+
         public MainWindow()
         {
             InitializeComponent();
-        
+
         }
 
-      
+
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            
-         
-            
-            lblInfo.Content="Windows loaded!";
 
-       
+
+
+            lblInfo.Content = "Windows loaded!";
+
+
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(Timer_Tick);
             timer.Interval = new TimeSpan(0, 0, 5);
@@ -127,27 +127,28 @@ namespace FidelioGuest
         private void Timer_Tick(object sender, EventArgs e)
         {
             lblInfo.Content = DateTime.Now.ToLongTimeString();
-         }
+        }
 
 
         private void ButCheck_Click(object sender, RoutedEventArgs e)
         {
             //butCheck.Content = txtCode.Text;
-            txtCode.Text = GetClipboardData();  
+            txtCode.Text = GetClipboardData();
         }
         public String GetClipboardData()
         {
 
             String returnString = "ничего нет";
 
-            
+
             if (Clipboard.ContainsData(DataFormats.Text))
             {
                 returnString = Clipboard.GetData(DataFormats.Text).ToString();
-            }           
+            }
             return returnString;
         }
 
-     
+
+
     }
 }
