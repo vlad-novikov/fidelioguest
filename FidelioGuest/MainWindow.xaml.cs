9ecb0ba707ec7bee6e91f810ad0f6f13b486b9b6
Vlad Novikov <vlad.novikov@gmail.com>
2020-01-29 18:44:25 +0300

    Succeeded with clipboard input. Added beta version of cli[board update event notification

--- FidelioGuest/MainWindow.xaml.cs
+++ FidelioGuest/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +12,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
 
 namespace FidelioGuest
 {
@@ -21,15 +24,97 @@ namespace FidelioGuest
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        public class ClipboardMonitorWindow : Window
+        {
+            private const int WM_CLIPBOARDUPDATE = 0x031D;
+
+            private IntPtr windowHandle;
+
+            public event EventHandler ClipboardUpdate;
+
+            protected override void OnInitialized(EventArgs e)
+            {
+                base.OnInitialized(e);
+
+                windowHandle = new WindowInteropHelper(this).EnsureHandle();
+                HwndSource.FromHwnd(windowHandle)?.AddHook(HwndHandler);
+                Start();
+            }
+
+            public static readonly DependencyProperty ClipboardUpdateCommandProperty =
+                DependencyProperty.Register("ClipboardUpdateCommand", typeof(ICommand), typeof(ClipboardMonitorWindow), new FrameworkPropertyMetadata(null));
+
+            public ICommand ClipboardUpdateCommand
+            {
+                get { return (ICommand)GetValue(ClipboardUpdateCommandProperty); }
+                set { SetValue(ClipboardUpdateCommandProperty, value); }
+            }
+
+            protected virtual void OnClipboardUpdate()
+            { }
+
+            public void Start()
+            {
+                NativeMethods.AddClipboardFormatListener(windowHandle);
+            }
+
+            public void Stop()
+            {
+                NativeMethods.RemoveClipboardFormatListener(windowHandle);
+            }
+
+            private IntPtr HwndHandler(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+            {
+                int a=1;
+                if (msg == WM_CLIPBOARDUPDATE)
+                {
+                    
+                    // fire event
+                    this.ClipboardUpdate?.Invoke(this, new EventArgs());
+                    // execute command
+                    if (this.ClipboardUpdateCommand?.CanExecute(null) ?? false)
+                    {
+                        this.ClipboardUpdateCommand?.Execute(null);
+                    }
+                    // call virtual method
+                    OnClipboardUpdate();
+                }
+                handled = false;
+                return IntPtr.Zero;
+            }
+
+
+            private static class NativeMethods
+            {
+                [DllImport("user32.dll", SetLastError = true)]
+                [return: MarshalAs(UnmanagedType.Bool)]
+                public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+                [DllImport("user32.dll", SetLastError = true)]
+                [return: MarshalAs(UnmanagedType.Bool)]
+                public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+            }
+
+
+
+        }
+        ///////////////////////////////////////////////
         
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        
         }
+
+      
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            
+         
+            
             lblInfo.Content="Windows loaded!";
 
        
@@ -56,13 +141,13 @@ namespace FidelioGuest
             String returnString = "ничего нет";
 
             
-            if (Clipboard.ContainsData("System.String"))
+            if (Clipboard.ContainsData(DataFormats.Text))
             {
-                returnString = Clipboard.GetData("System.String").ToString();
+                returnString = Clipboard.GetData(DataFormats.Text).ToString();
             }           
             return returnString;
         }
 
-        
+     
     }
 }
